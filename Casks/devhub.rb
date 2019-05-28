26db8675bf5520b236d0ae0933d80e62847e0e57
Bruno Lemos <brunohplemos@gmail.com>
2019-05-28 17:46:02 -0300

    v0.90.1

--- Casks/devhub.rb
+++ Casks/devhub.rb
@@ -1,6 +1,6 @@
 cask 'devhub' do
-  version '0.81.0'
-  sha256 '079b2926b46a661172638e60c8d4adec0a393e8344d7d1bb1735984c3f19cf9c'
+  version '0.90.1'
+  sha256 '00e4eeb167530e7695f56081adfdd11de7df4063b56d27e65d89d5b58f98be0c'
 
   url "https://github.com/devhubapp/devhub/releases/download/v#{version}/DevHub-#{version}.dmg"
   appcast 'https://github.com/devhubapp/devhub/releases.atom'
