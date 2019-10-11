1ed546ddab8172fb26ef305cebf8aa1f919af9e2
Dario Vladovic <d.vladimyr@gmail.com>
2019-10-11 22:49:31 +0200

    Update devhub to 0.97.1

--- Casks/devhub.rb
+++ Casks/devhub.rb
@@ -1,6 +1,6 @@
 cask 'devhub' do
-  version '0.95.0'
-  sha256 '0ed8e39ba6af47071c63d491ea79bcbe447d6b5b0db11ef81224fcccaca5a782'
+  version '0.97.1'
+  sha256 'a29d05c0877bb162620640fd4e4508b021cc250759dabc7087fa83378be01654'
 
   url "https://github.com/devhubapp/devhub/releases/download/v#{version}/DevHub-#{version}.dmg"
   appcast 'https://github.com/devhubapp/devhub/releases.atom'
